a867a1a5648771f37e2e57e680bc467cb674e97d
Daniil_Shurygin <Daniil_Shurygin@epam.com>
2019-06-11 14:14:59 +0400

    Task1: Fixes after review

--- task1/babel.config.js
+++ task1/babel.config.js
@@ -2,7 +2,7 @@ module.exports = api => {
     api.cache(true);
 
     return {
-        presets: ["@babel/preset-env"],
+        presets: ["@babel/env"],
         plugins: []
     };
 }
--- task1/src/1.2/index.js
+++ task1/src/1.2/index.js
@@ -1,23 +1,26 @@
 const csv = require('csvtojson');
 const fs = require('fs');
 
-const readableCsvStream = fs.createReadStream(`${__dirname}/csv/example.csv`);
-
-const createCsvParser = () => csv({
-    colParser:{
-        "amount": "omit",
-    },
-    checkType: true,
-});
+const logError = message => console.error(message);
+const withErrorBoundary = emitter => emitter.on('error', logError);
 
 const lowercaseHeader = (line, lineIndex) => {
     return lineIndex === 0 ? line.toLowerCase() : line;
 };
 
-const logJson = json => console.log(JSON.stringify(json));
+const readableCsvStream = withErrorBoundary(fs.createReadStream(`${__dirname}/csv/input.csv`));
+const writeableTxtStream = withErrorBoundary(fs.createWriteStream(`${__dirname}/csv/output.txt`));
 
-createCsvParser()
-    .on('error', console.error)
+const createCsvParser = () => csv({
+    colParser: {
+        amount: 'omit'
+    },
+    checkType: true
+});
+
+const setupCsvParser = () => createCsvParser()
     .fromStream(readableCsvStream)
     .preFileLine(lowercaseHeader)
-    .subscribe(logJson)
+    .pipe(writeableTxtStream);
+
+withErrorBoundary(setupCsvParser());
